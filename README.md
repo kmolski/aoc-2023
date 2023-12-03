937bfed02f72e5745994e3f5e1e8bf8fd356e3cf
kmolski <krzysztof.molski29@gmail.com>
2023-12-03 15:59:05 +0100

    Add solution for day 1

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # [Advent of Code 2023](https://adventofcode.com/2023) solutions
 
-My Advent of Code 2020 solutions in [Kotlin][kotlin], based on the [Advent of Code Kotlin Template][template] delivered by JetBrains.
+My Advent of Code 2023 solutions in [Kotlin][kotlin], based on the [AoC Kotlin Template][template] made by JetBrains.
+- [Day 1: Trebuchet?!](./src/Day01.kt)
+- [Day 2: Cube Conundrum](./src/Day02.kt)
 
 [kotlin]: https://kotlinlang.org
 [template]: https://github.com/kotlin-hands-on/advent-of-code-kotlin-template
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,50 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    val digit = Regex("([0-9])")
+    val digitOrWord = Regex("(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))")
+
+    fun part1(input: List<String>): Long {
+        return input.sumOf { parseLine(it, digit, String::toLong) }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        return input.sumOf { parseLine(it, digitOrWord, String::digitOrWordToLong) }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01.example")
+    check(part1(testInput) == 142L)
 
-    val input = readInput("Day01")
+    val testInput2 = readInput("Day01.example2")
+    check(part2(testInput2) == 281L)
+
+    val input = readInput("Day01.input")
     part1(input).println()
     part2(input).println()
+}
+
+fun parseLine(line: String, regex: Regex, parser: (String) -> Long): Long {
+    var firstDigit: String? = null
+    var lastDigit: String? = null
+    for (match in regex.findAll(line)) {
+        if (firstDigit == null) {
+            firstDigit = match.groupValues[1]
+        }
+        lastDigit = match.groupValues[1]
+    }
+    return 10 * parser(firstDigit!!) + parser(lastDigit!!)
+}
+
+fun String.digitOrWordToLong(): Long {
+    return when (this) {
+        "one"   -> 1
+        "two"   -> 2
+        "three" -> 3
+        "four"  -> 4
+        "five"  -> 5
+        "six"   -> 6
+        "seven" -> 7
+        "eight" -> 8
+        "nine"  -> 9
+        else -> this.toLong()
+    }
 }
