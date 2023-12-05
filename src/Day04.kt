231387aa1678abd4ca33bba27ab6995589e9f622
kmolski <krzysztof.molski29@gmail.com>
2023-12-05 18:15:16 +0100

    Add solution for day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,23 +1,46 @@
+import kotlin.math.pow
+
 fun main() {
 
-    val cardRecord = Regex("Card (\\d+): (.*)")
-    val subsetList = Regex("([^|]+)\\|?")
-    val subsetElem = Regex("(\\d+) *")
+    val cardRecord = Regex("Card(\\s+)(\\d+): (.*)")
+    val numberList = Regex("([^|]+)\\|?")
+    val whitespace = Regex("\\s+")
 
-    data class Token(val row: Int, val cols: IntRange, val value: String)
-
-    fun part1(input: List<String>): Long {
-        return 0L
+    data class Card(val id: Long, val winning: List<Long>, val hand: List<Long>) {
+        fun matchingNumbers() = winning.count { hand.contains(it) }
+        fun calculateScore() = 2.0.pow(matchingNumbers() - 1).toLong()
     }
 
-//    fun part2(input: List<String>): Long {
-//    }
+    fun parseNumbers(match: MatchResult): List<Long> {
+        return match.groupValues[1].trim().split(whitespace).map(String::toLong)
+    }
+
+    fun parseLine(line: String): Card {
+        val matches = cardRecord.matchEntire(line)!!
+        val numbers = numberList.findAll(matches.groupValues[3]).map(::parseNumbers).toList()
+        return Card(matches.groupValues[2].toLong(), numbers[0], numbers[1])
+    }
+
+    fun part1(input: List<String>): Long {
+        return input.map(::parseLine).sumOf(Card::calculateScore)
+    }
+
+    fun part2(input: List<String>): Long {
+        val cardNumbers = input.map(::parseLine)
+        val cards = cardNumbers.associateTo(HashMap()) { it.id to 1L }
+        for (card in cardNumbers) {
+            for (offset in 1..card.matchingNumbers()) {
+                cards.computeIfPresent(card.id + offset) { _, count -> count + cards[card.id]!! }
+            }
+        }
+        return cards.values.sum()
+    }
 
     val testInput = readInput("Day04.example")
-    check(part1(testInput) == 4361L)
-//    check(part2(testInput) == 467835L)
+    check(part1(testInput) == 13L)
+    check(part2(testInput) == 30L)
 
     val input = readInput("Day04.input")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
